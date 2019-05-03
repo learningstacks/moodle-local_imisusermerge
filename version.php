5b09ca2a21ce11eda787fdc5d90cad3030fd28cc
Terry Campbell <terry@learningstacks.com>
2019-05-03 09:16:00 -0500

    Update version

--- version.php
+++ version.php
@@ -11,8 +11,9 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->component = 'local_imisusermerge'; // Full name of the plugin (used for diagnostics)
-$plugin->version   = 2019040903;       // The current plugin version (Date: YYYYMMDDXX)
-$plugin->release = 'v1.0.0';
+$plugin->version   = 2019050300;       // The current plugin version (Date: YYYYMMDDXX)
+$plugin->release = 'v1.0.0-alpha-1';
+$plugin->maturity = MATURITY_ALPHA;
 $plugin->requires  = 2016120502;       // Requires this Moodle version
 $plugin->dependencies = array(
     'tool_mergeusers' => 2018030900
